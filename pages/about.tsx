696dc0c0dea9bd1e41c74b25d891cbe9301bd9b8
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2022-06-30 09:37:12 +0800

    accessories

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,12 +2,13 @@ import type { NextPage } from 'next';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 import Copyright from 'components/Copyright';
 import Link from 'components/Link';
 import ProTip from 'components/ProTip';
 
-const Home: NextPage = () => {
+const About: NextPage = () => {
   return (
     <Container maxWidth="lg">
       <Box
@@ -22,9 +23,11 @@ const Home: NextPage = () => {
         <Typography variant="h4" component="h1" gutterBottom>
           MUI v5 + Next.js with TypeScript example
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
+        <Box maxWidth="sm">
+          <Button variant="contained" component={Link} noLinkStyle href="/">
+            Go to the home page
+          </Button>
+        </Box>
         <ProTip />
         <Copyright />
       </Box>
@@ -32,4 +35,4 @@ const Home: NextPage = () => {
   );
 };
 
-export default Home;
+export default About;
